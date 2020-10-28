91ed354a829e603532ec7527693290d196b51580
osama <osama2390@gmail.com>
2020-10-27 22:54:07 -0400

    [Transaction] - Initial Commit - 10/27/2020

---
+++ Transaction.API/Models/FCTransaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transaction.API.Models
+{
+    public class FCTransaction
+    {
+        public string Name { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
