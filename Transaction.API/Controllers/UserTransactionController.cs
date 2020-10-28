21ee074701db7133f56ae697e8959f29665a9112
osama <osama2390@gmail.com>
2020-10-28 00:24:50 -0400

    Add TransactionController w/ Mock

---
+++ Transaction.API/Controllers/UserTransactionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Transaction.API.Infrastructure.Repositories;
+using Transaction.API.Model;
+
+namespace Transaction.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserTransactionController : ControllerBase
+    {
+        private readonly ILogger<UserTransactionController> _logger;
+        private readonly ITransactionRepository _transactionRepository;
+
+        public UserTransactionController(ILogger<UserTransactionController> logger, 
+                                         ITransactionRepository transactionRepository)
+        {
+            _logger = logger;
+            _transactionRepository = transactionRepository;
+        }
+
+
+        [HttpGet("GetTransactions")]
+        public IEnumerable<UserTransaction> GetTransactions([FromQuery] Guid UserId)
+        {
+            return _transactionRepository.GetTransactions(UserId);
+        }
+    }
+}
---
+++ Transaction.API/Infrastructure/Repositories/TransactionRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Transaction.API.Model;
+
+namespace Transaction.API.Infrastructure.Repositories
+{
+    public interface ITransactionRepository
+    {
+        public IEnumerable<UserTransaction> GetTransactions(Guid UserId);
+    }
+
+    public class MockTransactionRepository : ITransactionRepository
+    {
+        public IEnumerable<UserTransaction> GetTransactions(Guid UserId)
+        {
+            //TODO: Refactor into service layer
+            var userId = new Guid("79e814c5-31e8-4a91-8c97-8c57cfccbc33");
+            var userId2 = new Guid("c75fdddf-183b-493b-a77f-d0cb7817ec2e");
+
+            var mockTransactions =  new List<UserTransaction>()
+            {
+                new UserTransaction()
+                {
+                    UserId = userId,
+                    Name = "Test 1",
+                    Amount = -10m
+                },
+                new UserTransaction()
+                {
+                    UserId = userId,
+                    Name = "Test 2",
+                    Amount = 25m
+                },
+                 new UserTransaction()
+                {
+                    UserId = userId2,
+                    Name = "Test 2",
+                    Amount = 25m
+                }
+            };
+
+            return mockTransactions.Where(x => x.UserId == UserId).ToList();
+        }
+    }
+}
