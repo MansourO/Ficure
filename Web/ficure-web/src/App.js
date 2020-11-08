fe3f53c2491fbe6ec4f7c1951ea492dfdc76fb55
osama <osama2390@gmail.com>
2020-11-08 14:25:57 -0500

    Remove React Project

--- Web/ficure-web/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TransactionList from './components/Transactions/TransactionList';
-
-const jsx = (
-    <TransactionList />
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
